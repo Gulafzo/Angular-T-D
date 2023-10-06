7b1e97fc9bfcac1d32d90f549e16c23f06d4a5a2
Гулафзо <mega.gulafzo@mail.ru>
2023-10-07 00:04:01 +0300

    1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppTowerComponent } from './app-tower/app-tower.component';
+import { AppOppositionComponent } from './app.opposition/app.opposition.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppTowerComponent,
+    AppOppositionComponent
   ],
   imports: [
     BrowserModule
